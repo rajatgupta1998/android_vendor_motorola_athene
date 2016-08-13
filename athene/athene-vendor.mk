1b00115e93efdde643d44951c4ea0af4ec19e7be
Vache <contact@ache.mobi>
2016-08-13 09:18:38 +0200

    athene: Switch to prebuilt GPS

--- athene/athene-vendor.mk
+++ athene/athene-vendor.mk
@@ -116,6 +116,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/athene/proprietary/lib/hw/audio_policy.default.so:system/lib/hw/audio_policy.default.so \
     vendor/motorola/athene/proprietary/lib/hw/camera.msm8952.so:system/lib/hw/camera.msm8952.so \
     vendor/motorola/athene/proprietary/lib/hw/fingerprint.msm8952.so:system/lib/hw/fingerprint.msm8952.so \
+    vendor/motorola/athene/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so.so \
     vendor/motorola/athene/proprietary/lib/hw/sensorhub.msm8952.so:system/lib/hw/sensorhub.msm8952.so \
     vendor/motorola/athene/proprietary/lib/hw/sensors.msm8952.so:system/lib/hw/sensors.msm8952.so \
     vendor/motorola/athene/proprietary/lib/lib_fpc_tac_shared.so:system/lib/lib_fpc_tac_shared.so \
